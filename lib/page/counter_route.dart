fc462e18e3cab20d74d63e809d8c7db88ac604fc
YoshikazuHosono <yyhhwork@gmail.com>
2022-02-05 22:31:10 +0900

    counterをflutter_hooksにしてみる

--- lib/page/counter_route.dart
+++ lib/page/counter_route.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-final counterProvider = StateProvider((ref) => 0);
-
-class Counter extends ConsumerWidget {
+class Counter extends HookWidget {
   const Counter({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final counterWatch = ref.watch(counterProvider.state);
-    final counterRead = ref.read(counterProvider.state);
+  Widget build(BuildContext context) {
+
+    var count = useState(0);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Counter example')),
@@ -17,20 +15,20 @@ class Counter extends ConsumerWidget {
         // Consumer is a widget that allows you reading providers.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('count:' + counterWatch.state.toString())],
+          children: [Text('count:' + count.value.toString())],
         ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FloatingActionButton(
-            onPressed: () => counterRead.state++,
+            onPressed: () => count.value++,
             tooltip: 'Add',
             child: const Icon(Icons.add),
             heroTag: "hero1", // TODO いや結局これ何よ
           ),
           FloatingActionButton(
-            onPressed: () => counterRead.state = 0,
+            onPressed: () => count.value = 0,
             tooltip: 'Reset',
             child: const Icon(Icons.remove),
             heroTag: "hero2",
